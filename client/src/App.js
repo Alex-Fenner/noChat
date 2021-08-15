ba6c185b7bd5456638bc7a6769220752ba217755
Alex-Fenner <alexfennerg@gmail.com>
2021-08-15 11:05:00 +0100

    basic chat interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-import socketIOClient from "socket.io-client";
-import {useEffect,useState} from 'react';
-const ENDPOINT = "http://localhost:3001";
-const socket = socketIOClient.connect(ENDPOINT);
+import TextField from "@material-ui/core/TextField"
+import React, { useEffect, useRef, useState } from "react"
+import io from "socket.io-client"
+import "./App.css"
+import { css } from '@emotion/css';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 function App() {
-  const [fake,setFake]=useState(false)
-  
+  const [state, setState] = useState({ message: "", name: "" })
+  const [chat, setChat] = useState([])
 
-  useEffect(() => {
-    socket.on("recieve_tick", ()=>recievedTick());
-  }, [])
+  const socketRef = useRef()
+  const ROOT_CSS = css({
+    height: '500px',
+    width: '300px'
+  });
 
-  const recievedTick=()=>{
-    console.log("recieved ticks")
-    setFake(!fake)
+  useEffect(
+    () => {
+      socketRef.current = io.connect("http://localhost:3001")
+      socketRef.current.on("message", ({ name, message }) => {
+        setChat([...chat, { name, message }])
+      })
+      return () => socketRef.current.disconnect()
+    },
+    [chat]
+  )
+
+  const onTextChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value })
   }
 
-  const sendTick=()=>{
-    socket.emit('send_tick',[])
+  const onMessageSubmit = (e) => {
+    const { name, message } = state
+    socketRef.current.emit("message", { name, message })
+    e.preventDefault()
+    setState({ message: "", name })
+  }
+
+  const renderChat = () => {
+    return chat.map(({ name, message }, index) => (
+      <div class="card2" key={index}>
+        <h6>{name}</h6>
+      <div class="container">
+        
+        <p>{message}</p>
+      </div>
+    </div>
+
+    ))
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <input type="checkbox" Checked={fake} onClick={()=>sendTick()}></input>
-      </header>
+    <div className="card">
+      <div className="render-chat">
+        <ScrollToBottom className={ROOT_CSS}>
+          {renderChat()}
+        </ScrollToBottom>
+        <div className="footer">
+          <div className="name-field">
+            <TextField name="name" onChange={(e) => onTextChange(e)} value={state.name} label="Name" />
+          </div>
+          <div>
+            <TextField
+              name="message"
+              onChange={(e) => onTextChange(e)}
+              value={state.message}
+              id="outlined-multiline-static"
+              variant="outlined"
+              label="Message"
+            />
+          </div>
+          <button onClick={onMessageSubmit}>Send Message</button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- server/index.js
+++ server/index.js
@@ -16,14 +16,19 @@ const io = socketIo(server);
 
 
 io.on("connection", socket => {
-    console.log("New client connected");
+    //console.log("New client connected");
 
+    socket.on('message', ({ name, message }) => {
+        io.emit('message', { name, message })
+      })
 
     socket.on("incoming data", (data)=>{
        socket.broadcast.emit("outgoing data", {num: data});
     });
 
-    socket.on("disconnect", () => console.log("Client disconnected"));
+    socket.on("disconnect", () => {
+        
+    });
     socket.on("send_tick", () => {
         console.log("tick recieved")
         io.emit("recieve_tick",[])
